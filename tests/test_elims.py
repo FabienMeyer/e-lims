ca45aba5de1607b26ee1428c45243a2da457a74d
Fabien Meyer <fabien-meyer@outlook.com>
2023-09-06 19:09:48 +0200

    tests: addd base tests

---
+++ tests/test_elims.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""elims tests."""
+
+import pytest
+
+from elims import elims
+
+
+@pytest.fixture
+def name() -> None:
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    return 'World'
+
+
+def test_content(name: str) -> None:
+    """Sample pytest test function with the pytest fixture as an argument."""
+    assert 'Hello World' in elims.hello(name)
