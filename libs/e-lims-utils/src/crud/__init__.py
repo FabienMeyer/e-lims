d04d3c155525aa402469f2d210cf42d0cb3be5a1
Fabien Meyer <fabien-meyer@outlook.com>
2024-02-11 11:41:32 +0100

    docs: Add Crud documentation

--- libs/e-lims-utils/src/crud/__init__.py
+++ libs/e-lims-utils/src/crud/__init__.py
@@ -1,1 +1,12 @@
-"""e-lims-utils crud module."""
+"""This module contains the Crud class and the BaseSqlModel class.
+
+The Crud class provides methods to create, read, update, and delete (CRUD) records in a database table.
+It uses SQLAlchemy's Session and Engine to interact with the database.
+
+The BaseSqlModel class serves as a base for all SQLModel classes in the application.
+It includes a primary key field named 'uid'.
+
+Classes:
+    BaseSqlModel: A base class for all SQLModel classes in the application.
+    Crud: A class to perform CRUD operations on a SQLModel.
+"""
